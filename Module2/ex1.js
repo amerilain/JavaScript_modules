059e8256855480e76f193afffb9d64062c3007a5
amerilain <amerilain@gmail.com>
2022-11-28 16:40:05 +0200

    ex1 done

---
+++ Module2/ex1.js
@@ -0,0 +1,11 @@
+'use strict';
+let numbers = [];
+numbers[0] = +prompt("What is your first number?");
+numbers[1] = +prompt("What is your second number?");
+numbers[2] = +prompt("What is your third number?");
+numbers[3] = +prompt("What is your fourth number?");
+numbers[4] = +prompt("What is your fifth number?");
+
+for (let a = 4; a >= 0; a--){
+    console.log(numbers[a]);
+    }
